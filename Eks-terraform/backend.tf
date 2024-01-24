88275aa2cdd32d45dc69a0a1e559c7ed77159d26
RezHCloud <151577948+RezHCloud@users.noreply.github.com>
2024-01-24 14:54:23 +0000

    backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "reztetrisstate" # Replace with your actual S3 bucket name
-    key    = "Jenkins/EKSterraform.tfstate"
+    key    = "Jenkins/terraform.tfstate"
     region = "eu-west-2"
   }
 }
